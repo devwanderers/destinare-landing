e849e723cc803ff0b09ba19352d05cbc2fbe271e
brandiniA <90206743+brandiniA@users.noreply.github.com>
2021-11-01 17:19:50 -0600

    added selectors

--- src/store/reducers/auth/selectors.js
+++ src/store/reducers/auth/selectors.js
@@ -1,1 +1,16 @@
-export default {}
+import { createSelector } from 'reselect'
+
+export const authReducerSelector = createSelector(
+    (state) => state.auth,
+    (authReducer) => authReducer
+)
+
+export const authenticatedSelector = createSelector(
+    authReducerSelector,
+    (authReducer) => authReducer.auth
+)
+
+export const userDataSelector = createSelector(
+    authReducerSelector,
+    (authReducer) => authReducer.userData
+)
