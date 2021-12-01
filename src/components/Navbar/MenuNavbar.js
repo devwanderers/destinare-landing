d30bc59e4a7f9d2a7beb4993ccfa2bbbd04dcffb
brandiniA <90206743+brandiniA@users.noreply.github.com>
2021-12-01 08:06:08 -0600

    Removed Button from mobile menu

--- src/components/Navbar/MenuNavbar.js
+++ src/components/Navbar/MenuNavbar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Menu } from 'antd'
 // import { useHistory } from 'react-router'
-import { AiOutlineLogout, AiOutlineLogin } from 'react-icons/ai'
+// import { AiOutlineLogout, AiOutlineLogin } from 'react-icons/ai'
 import { IoMdAirplane } from 'react-icons/io'
 import { BsBook } from 'react-icons/bs'
 // import { LoginInPath } from '../../constants/routerConstants'
@@ -30,34 +30,34 @@ const menusMobile = [menusOptions.whitePaper, menusOptions.travel]
 const MenuNavbar = ({ authenticated, isMobile, logout }) => {
     // const history = useHistory()
     const _menu = !isMobile ? menus : menusMobile
-    let logOutItem =
-        !authenticated && isMobile ? (
-            <Menu.Item className="">
-                <div
-                    className="flex flex-row items-center"
-                    onClick={() =>
-                        windowOpen(
-                            'https://members.tripvixia.com/membership/signup?invite=0e3cb515-2949-4e1a-bb83-fd7fe4a5b251&referraltype=3'
-                        )
-                    }
-                >
-                    <AiOutlineLogin className="mr-2 relative -mt-1" /> Get
-                    Started
-                </div>
-            </Menu.Item>
-        ) : (
-            <Menu.Item className="">
-                <div
-                    className="flex flex-row items-center"
-                    onClick={() => logout()}
-                >
-                    <AiOutlineLogout className="mr-2 relative -mt-1" /> Log out
-                </div>
-            </Menu.Item>
-        )
-    if (!isMobile && !authenticated) {
-        logOutItem = null
-    }
+    // let logOutItem =
+    //     !authenticated && isMobile ? (
+    //         <Menu.Item className="">
+    //             <div
+    //                 className="flex flex-row items-center"
+    //                 onClick={() =>
+    //                     windowOpen(
+    //                         'https://members.tripvixia.com/membership/signup?invite=0e3cb515-2949-4e1a-bb83-fd7fe4a5b251&referraltype=3'
+    //                     )
+    //                 }
+    //             >
+    //                 <AiOutlineLogin className="mr-2 relative -mt-1" /> Get
+    //                 Started
+    //             </div>
+    //         </Menu.Item>
+    //     ) : (
+    //         <Menu.Item className="">
+    //             <div
+    //                 className="flex flex-row items-center"
+    //                 onClick={() => logout()}
+    //             >
+    //                 <AiOutlineLogout className="mr-2 relative -mt-1" /> Log out
+    //             </div>
+    //         </Menu.Item>
+    //     )
+    // if (!isMobile && !authenticated) {
+    //     logOutItem = null
+    // }
 
     return (
         <Menu className="border-r-0 -mx-24px">
@@ -78,7 +78,7 @@ const MenuNavbar = ({ authenticated, isMobile, logout }) => {
                 )
             })}
             <Menu.Divider className="flex" />
-            {logOutItem}
+            {/* {logOutItem} */}
         </Menu>
     )
 }
