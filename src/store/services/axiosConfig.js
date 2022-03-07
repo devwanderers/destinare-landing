7bdf0cc88f5be0f272850524959e0572aec61d10
brandiniA <90206743+brandiniA@users.noreply.github.com>
2022-03-07 17:10:05 -0600

    new build

--- src/store/services/axiosConfig.js
+++ src/store/services/axiosConfig.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 const axiosInstance = axios.create({
     baseURL:
         process.env.NODE_ENV === 'production'
-            ? 'http://localhost:3000/'
+            ? 'https://destinare.io/api-destinare'
             : 'http://localhost:3000/',
 })
 
