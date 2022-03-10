22faabe5ae7dfcf9a87a63a661ca824e44b4d83c
Brandon Alonso Salinas Rangel <bsalinas@cappaglobal.com>
2022-03-09 20:57:27 -0600

    build

---
+++ build/static/js/13.4f06cccd.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpdestinare=this.webpackJsonpdestinare||[]).push([[13],{232:function(e,s,c){"use strict";c.r(s);c(1);var t=c(5),n=function(){return Object(t.jsx)("div",{className:"css-generic w-full min-h-screen justify-center items-center",children:Object(t.jsx)("div",{className:"css-generic text-black-1 mx-auto text-8xl",children:"Error 404"})})};s.default=function(e){return Object(t.jsx)("div",{className:"css-generic w-full min-h-screen justify-center items-center",children:Object(t.jsx)(n,{})})}}}]);
+//# sourceMappingURL=13.4f06cccd.chunk.js.map
--- build/static/js/13.d7cf864c.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpdestinare=this.webpackJsonpdestinare||[]).push([[13],{230:function(s,e){}}]);
-//# sourceMappingURL=13.d7cf864c.chunk.js.map
